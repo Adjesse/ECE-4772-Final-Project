ecd078fc0a0cbe5df694d9061989b54994f3443e
Jesse <Adam@tracetronic.local>
2023-11-09 10:48:14 -0500

    Added read of txt file

--- Final.cpp
+++ Final.cpp
@@ -1,1 +1,23 @@
 using namespace std; 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main() {
+
+    std::ifstream inputFile("data.txt"); // Open the file
+
+    if (inputFile.is_open()) {
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            std::cout << line << std::endl; // Print each line to the console
+        }
+
+        inputFile.close(); // Close the file when you're done with it
+    } else {
+        std::cerr << "Unable to open file" << std::endl;
+    }
+    return 0;
+}
+
