774cf51b9992432254c82f30e5e4bcbceff1f137
Jesse <Adam@tracetronic.local>
2023-11-09 18:10:27 -0500

    Test complete Data correct

--- Final.cpp
+++ Final.cpp
@@ -43,12 +43,19 @@ int main() {
     }
     
     int count = 0;
-    
+    int count1 = 0;
+    int count2 = 0;
+    int count3 = 0;
+    int count4 = 0;
+    Engine_Speed.Data_Length = 0;
+    Vehicle_Speed.Data_Length = 0;
+    ECT.Data_Length = 0;
+    Fuel_Percent.Data_Length = 0;
 
 
     while (file >> TEMP_DATA_Input.timestamp >> TEMP_DATA_Input.PID >> TEMP_DATA_Input.Data) {
         // Assuming your struct has a public array and you want to store each entry
-
+    
         if (TEMP_DATA_Input.PID == 12)
         {
         Engine_Speed.Data_Length ++;
@@ -78,13 +85,20 @@ int main() {
         count++;
 
         // You may want to check if count exceeds the array size to avoid overflow
-        if (count >= n) {
-            cout << "Size exceeded. More entries than parameter n. Increase the size or handle accordingly." << endl;
+        if (count > n) {
+            cout << "First Size exceeded. More entries than parameter n. Increase the size or handle accordingly." << endl;
             break;
         }
     }
     
     file.close();
+
+    cout << Engine_Speed.Data_Length << endl;
+    cout << Vehicle_Speed.Data_Length << endl;
+    cout << ECT.Data_Length << endl;
+    cout << Fuel_Percent.Data_Length << endl;
+    cout << count << endl;
+
     
     
     
@@ -119,52 +133,53 @@ int main() {
     if (Fuel_Percent.PID == NULL) {printf("error");}
     if (Fuel_Percent.Data == NULL) {printf("error");}
     
-
-    tmp_data TEMP_DATA_Input;
   
+    count = 0;
+   
+     ifstream file1(filename);
 
-    const char* filename = "data.txt";  // Change this to your file name
-
-    ifstream file(filename);
-
-    if (!file.is_open()) {
+    if (!file1.is_open()) {
         cout << "Error opening file." << endl;
         return 1;
     }
     
-    int count = 0;
+
     
 
 
-    while (file >> TEMP_DATA_Input.timestamp >> TEMP_DATA_Input.PID >> TEMP_DATA_Input.Data) {
+    while (file1 >> TEMP_DATA_Input.timestamp >> TEMP_DATA_Input.PID >> TEMP_DATA_Input.Data) {
         // Assuming your struct has a public array and you want to store each entry
 
         if (TEMP_DATA_Input.PID == 12)
         {
-        Engine_Speed.timestamp[count] = TEMP_DATA_Input.timestamp;
-        Engine_Speed.PID[count] = TEMP_DATA_Input.PID;
-        Engine_Speed.Data[count] = TEMP_DATA_Input.Data;
+        Engine_Speed.timestamp[count1] = TEMP_DATA_Input.timestamp;
+        Engine_Speed.PID[count1] = TEMP_DATA_Input.PID;
+        Engine_Speed.Data[count1] = TEMP_DATA_Input.Data;
+        count1 ++;
         }
 
         else if (TEMP_DATA_Input.PID == 13)
         {
-        Vehicle_Speed.timestamp[count] = TEMP_DATA_Input.timestamp;
-        Vehicle_Speed.PID[count] = TEMP_DATA_Input.PID;
-        Vehicle_Speed.Data[count] = TEMP_DATA_Input.Data;
+        Vehicle_Speed.timestamp[count2] = TEMP_DATA_Input.timestamp;
+        Vehicle_Speed.PID[count2] = TEMP_DATA_Input.PID;
+        Vehicle_Speed.Data[count2] = TEMP_DATA_Input.Data;
+        count2 ++;
         }
         
         else if (TEMP_DATA_Input.PID == 103)
         {
-        ECT.timestamp[count] = TEMP_DATA_Input.timestamp;
-        ECT.PID[count] = TEMP_DATA_Input.PID;
-        ECT.Data[count] = TEMP_DATA_Input.Data;
+        ECT.timestamp[count3] = TEMP_DATA_Input.timestamp;
+        ECT.PID[count3] = TEMP_DATA_Input.PID;
+        ECT.Data[count3] = TEMP_DATA_Input.Data;
+        count3 ++;
         }
 
         else if (TEMP_DATA_Input.PID == 47)
         {
-        Fuel_Percent.timestamp[count] = TEMP_DATA_Input.timestamp;
-        Fuel_Percent.PID[count] = TEMP_DATA_Input.PID;
-        Fuel_Percent.Data[count] = TEMP_DATA_Input.Data;
+        Fuel_Percent.timestamp[count4] = TEMP_DATA_Input.timestamp;
+        Fuel_Percent.PID[count4] = TEMP_DATA_Input.PID;
+        Fuel_Percent.Data[count4] = TEMP_DATA_Input.Data;
+        count4 ++;
         }
         
         else
@@ -176,19 +191,31 @@ int main() {
         count++;
 
         // You may want to check if count exceeds the array size to avoid overflow
-        if (count >= n) {
+        if (count > n) {
             cout << "Size exceeded. More entries than parameter n. Increase the size or handle accordingly." << endl;
             break;
         }
     }
     
-    file.close();
+    file1.close();
 
     // Now Engine_Speed contains your data
 
-    // Do something with the data, like printing it for verification
+    //Do something with the data, like printing it for verification
+    for (int i = 0; i < Engine_Speed.Data_Length; i++) {
+       cout << "Timestamp: " << Engine_Speed.timestamp[i] << ", Identifier: " << Engine_Speed.PID[i]  << ", Data: " << Engine_Speed.Data[i]  << endl;
+    }
+
     for (int i = 0; i < Vehicle_Speed.Data_Length; i++) {
-        cout << "Timestamp: " << Vehicle_Speed.timestamp[i] << ", Identifier: " << Vehicle_Speed.PID[i]  << ", Data: " << Vehicle_Speed.Data[i]  << endl;
+       cout << "Timestamp: " << Vehicle_Speed.timestamp[i] << ", Identifier: " << Vehicle_Speed.PID[i]  << ", Data: " << Vehicle_Speed.Data[i]  << endl;
+    }
+
+    for (int i = 0; i < ECT.Data_Length; i++) {
+       cout << "Timestamp: " << ECT.timestamp[i] << ", Identifier: " << ECT.PID[i]  << ", Data: " << ECT.Data[i]  << endl;
+    }
+
+    for (int i = 0; i < Fuel_Percent.Data_Length; i++) {
+       cout << "Timestamp: " << Fuel_Percent.timestamp[i] << ", Identifier: " << Fuel_Percent.PID[i]  << ", Data: " << Fuel_Percent.Data[i]  << endl;
     }
 
     return 0;
