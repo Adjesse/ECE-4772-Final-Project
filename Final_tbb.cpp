ec136e01d1720d5eb779e272333bd2cf1713a059
trey-OU-2024 <trey.plichta@gmail.com>
2023-12-10 00:04:03 -0500

    f memory leaks

--- Final_tbb.cpp
+++ Final_tbb.cpp
@@ -600,29 +600,65 @@ int main(int argc, char **argv) {
     // for (int i = 0; i < Fuel_Percent.Data_Length; i++) {
     //    cout << "Timestamp: " << Fuel_Percent.timestamp[i] << ", Identifier: " << Fuel_Percent.PID[i]  << ", Data: " << Fuel_Percent.Data[i]  << endl;
     // }
+
+    cout << "1" << endl;
     free(engine_speed_hp);
     for(int i = 0; i < nt; i++)
     {
         free(engine_speed_hp[i]);
     }
-
+    cout << "1" << endl;
     free(vehicle_speed_hp);
     for(int i = 0; i < nt; i++)
     {
         free(vehicle_speed_hp[i]);
     }
-
+    cout << "1" << endl;
     for(int i = 0; i < number_of_rows; i++)
     {
         free(A[i]);
     }
     free(A);
-    free(Engine_Speed.timestamp); free(Engine_Speed.PID); free(Engine_Speed.Data); 
+    cout << "1" << endl;
+    
+
+    // Delete memory for Engine_Speed
+    delete[] Engine_Speed.Data;
+    delete[] Engine_Speed.PID;
+    delete[] Engine_Speed.timestamp;
+    Engine_Speed.timestamp;
+
+    // Delete memory for Vehicle_Speed
+    delete[] Vehicle_Speed.Data;
+    delete[] Vehicle_Speed.PID;
+    delete[] Vehicle_Speed.timestamp;
+
+    // Delete memory for ECT
+    delete[] ECT.Data;
+    delete[] ECT.PID;
+    delete[] ECT.timestamp;
+
+    // Output "1"
+    cout << "1" << endl;
+
+    // Delete memory for Fuel_Percent
+    delete[] Fuel_Percent.Data;
+    delete[] Fuel_Percent.PID;
+    delete[] Fuel_Percent.timestamp;
+
+    // Delete memory for Distance_Since_Clear
+    delete[] Distance_Since_Clear.Data;
+    delete[] Distance_Since_Clear.PID;
+    delete[] Distance_Since_Clear.timestamp;
+
+    /*free(Engine_Speed.timestamp); free(Engine_Speed.PID); free(Engine_Speed.Data); 
     free(Vehicle_Speed.timestamp); free(Vehicle_Speed.PID); free( Vehicle_Speed.Data);
     free(ECT.timestamp); free(ECT.PID); free(ECT.Data);
+    cout << "1" << endl;
     free(Fuel_Percent.timestamp); free(Fuel_Percent.PID); free(Fuel_Percent.Data);   
-    free(Distance_Since_Clear.timestamp); free(Distance_Since_Clear.PID); free(Distance_Since_Clear.Data);
-    free(engine_speed_h); free(vehicle_speed_h);    
+    free(Distance_Since_Clear.timestamp); free(Distance_Since_Clear.PID); free(Distance_Since_Clear.Data);*/
+    free(engine_speed_h); free(vehicle_speed_h); 
+    cout << "1" << endl;   
      
 
     return 0;
