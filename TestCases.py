ec0ac2119e19735467cd5a48647bd3fc84a20796
Adam Jesse <adam.jesse23@gmail.com>
2023-12-14 15:57:53 -0500

    Adam Update 3:57

--- TestCases.py
+++ TestCases.py
@@ -29,11 +29,10 @@ def function(test_number,cpp_program_path,cpp_arguments):
         #print(data)
 try:
 
-#Test n mult 1-10
-    for test in range(1,11):
-        
-        cpp_arguments_seq = [str(test)]
-        function(test,cpp_program_path_seq ,cpp_arguments_seq)
+    # function("20",cpp_program_path_seq ,["20"])
+    # function("50",cpp_program_path_seq ,["50"])
+
+    
 # #Test n mult 1-10
 #     for test in range(1,11):
 #         cpp_arguments_tbb = [str(test) , "1"]
@@ -44,13 +43,17 @@ try:
 #     function("12",cpp_program_path_tbb,['50','1'])
 
 #Test Partial Histogram 1-10
-    # loop = 12
-    # for nmult in range(1,11):
-    #     for hp in range(1,11):
-    #         cpp_arguments_tbb = [str(nmult) , str(hp)]
-    #         test_number = str(loop)
-    #         function(test_number,cpp_program_path_tbb,cpp_arguments_tbb)  
-    #         loop = loop + 1 
+    loop = 1
+    for nmult in range(1,3):
+        if nmult == 1:
+             n_mult = "20"
+        else:
+             n_mult = "50" 
+        for hp in range(1,11):
+            cpp_arguments_tbb = [n_mult, str(hp)]
+            test_number = str(loop)
+            function(test_number,cpp_program_path_tbb,cpp_arguments_tbb)  
+            loop = loop + 1 
 
 
     df = pd.DataFrame(data)
